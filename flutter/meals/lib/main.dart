a8fd0b8d7b6332f3431b0216322957cdfccb6609
Paulo Bandeira <paulounisul@gmail.com>
2021-08-21 08:50:44 -0300

    Passando Dados via Construtor->Ok, Tema & Stylo ->Ok, Navegando para uma Nova Tela->Ok

---
+++ flutter/meals/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './screens/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vamos Cozinhar?',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          fontFamily: 'Raleway',
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                ),
+              )),
+      home: CategoriesScreen(),
+    );
+  }
+}
