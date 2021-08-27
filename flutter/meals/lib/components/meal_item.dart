4724f95ca7e3df02671de10ced4e84c509af16e9
Paulo Bandeira <paulounisul@gmail.com>
2021-08-27 07:42:51 -0300

    Navegando para tela de Detalhe da Refeição -> Incluiu Rotas, Novo Componente,

--- flutter/meals/lib/components/meal_item.dart
+++ flutter/meals/lib/components/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import '../utils/app_routes.dart';
 import '../models/meals.dart';
 
 class MealItem extends StatelessWidget {
@@ -7,12 +8,17 @@ class MealItem extends StatelessWidget {
 
   const MealItem(this.meal);
 
-  void _selectMeal() {}
+  void _selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      AppRoutes.MEAL_DETAIL,
+      arguments: meal,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: _selectMeal,
+      onTap: () => _selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
