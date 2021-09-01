f6467686ba5090d21c515b2783400db68e1ba494
Paulo Bandeira <paulounisul@gmail.com>
2021-09-01 08:11:29 -0300

    Adicionado TabBar na AppBar ->Ok, tabBar,TabBarView,rotas

--- flutter/meals/lib/main.dart
+++ flutter/meals/lib/main.dart
@@ -3,6 +3,7 @@ import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'utils/app_routes.dart';
+import 'screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
       //home: CategoriesScreen(),
       //initialRoute: <rota inicial>, pode ser usado também.
       routes: {
-        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
