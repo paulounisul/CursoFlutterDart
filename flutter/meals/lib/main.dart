547158f30418fac52d9c570ab4e10b76254a7d28
Paulo Bandeira <paulounisul@gmail.com>
2021-09-07 07:59:05 -0300

    links no Drawer e Substituindo telas ->Ok pushNamed, pushReplacementNamed,navigator

--- flutter/meals/lib/main.dart
+++ flutter/meals/lib/main.dart
@@ -4,6 +4,7 @@ import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'utils/app_routes.dart';
 import 'screens/tabs_screen.dart';
+import 'screens/settings_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
         AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
       },
       //Metodos para validar ou criar rotas dinamicamente...
       // onGenerateRoute: (settings) {
