6fd8e2182d8e7e8a6b93b118057387f33829b992
Paulo Bandeira <paulounisul@gmail.com>
2021-09-21 07:45:21 -0300

    Estilizando APP ->Ok. GridView, gridDelegate, ClipRRect,GridTile,GridTileBar,IconButton, Theme

---
+++ flutter/shop/lib/pages/product_detail_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductDetailPage extends StatelessWidget {
+  final Product product;
+
+  const ProductDetailPage({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
+    );
+  }
+}
