cb0ec0d33cb03ba19c3ad6821c705ce6ee3f78b2
Paulo Bandeira <paulounisul@gmail.com>
2021-10-06 07:49:22 -0300

    ChangeNotifierProvider.value -> Refatorar, ChangeNotifierProvider.value/

--- flutter/shop/lib/components/product_grid.dart
+++ flutter/shop/lib/components/product_grid.dart
@@ -12,8 +12,12 @@ class ProductGrid extends StatelessWidget {
 
     return GridView.builder(
       itemCount: loadedProducts.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (_) => loadedProducts[i],
+      //Refatorando, é necessário utilizar o Singleton,
+      //pra evitarr que em alguma instancia seja criado um novo
+      //Objecto De ChangeNotifier.. create (passa para) Value.
+      //e não passar a função apenas chamar loadedProduct.
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: loadedProducts[i],
         child: ProductItem(),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
