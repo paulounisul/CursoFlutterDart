ef93d6981d4bc8d9d1294354459c404b9a1077fe
Paulo Bandeira <paulounisul@gmail.com>
2021-11-12 11:36:09 -0300

    Fazer Upgrade Windows.

--- flutter/shop/lib/pages/product_form_page.dart
+++ flutter/shop/lib/pages/product_form_page.dart
@@ -11,6 +11,17 @@ class _ProductFormPageState extends State<ProductFormPage> {
   @override
   Widget build(BuildContext context) {
     final _priceFocus = FocusNode();
+    final _descriptioFocus = FocusNode();
+    final _ImageUrlFocus = FocusNode();
+    final _imageUrlController = TextEditingController();
+
+    @override
+    void dispose() {
+      super.dispose();
+      _priceFocus.dispose();
+      _descriptioFocus.dispose();
+      _ImageUrlFocus.dispose();
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -38,6 +49,48 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 keyboardType: TextInputType.numberWithOptions(
                   decimal: true,
                 ),
+                // quando precisar solicitar o focus em um determinado
+                // elemento do form.
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptioFocus);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Descrição'),
+                textInputAction: TextInputAction.next,
+                focusNode: _descriptioFocus,
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Url da Imagem'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.url,
+                      focusNode: _ImageUrlFocus,
+                      controller: _imageUrlController,
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: const EdgeInsets.only(
+                      top: 10,
+                      left: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text('Informe a Url'),
+                  ),
+                ],
               ),
             ],
           ),
