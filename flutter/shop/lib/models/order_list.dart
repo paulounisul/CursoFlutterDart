bb4d764bf3f7cb4c497c0483eed50ccce07d84d8
Paulo Bandeira <paulounisul@gmail.com>
2022-01-11 08:09:10 -0300

    carregando os Pedidos ->OK. DateTime/CartItem/List<dinamic>/toList()/Map<string,dinamic>/

--- flutter/shop/lib/models/order_list.dart
+++ flutter/shop/lib/models/order_list.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/utils/constantes.dart';
 import 'cart.dart';
 import 'order.dart';
@@ -17,11 +18,42 @@ class OrderList with ChangeNotifier {
     return _items.length;
   }
 
+  //Refatorando lista de produtos. de dummydata para FireBase.
+  Future<void> loadOrders() async {
+    _items.clear();
+    final response = await http.get(
+      Uri.parse('${Constants.ORDER_BASE_URL}.json'),
+    );
+
+    if (response.body == 'null') return;
+    //else faz o load
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(
+        Order(
+          id: orderId,
+          date: DateTime.parse(orderData['date']),
+          total: orderData['total'],
+          products: (orderData['products'] as List<dynamic>).map((item) {
+            return CartItem(
+              id: item['id'],
+              productId: item['productId'],
+              name: item['name'],
+              quantity: item['quantity'],
+              price: item['price'],
+            );
+          }).toList(),
+        ),
+      );
+    });
+
+    notifyListeners();
+  }
+
   /**
    * pra jogar o pedido pra nuvem. Vamos transformar o addOrder em uma 
    * future
   */
-
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
