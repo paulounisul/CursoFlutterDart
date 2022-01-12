b6b59c24d481bccff57c796c28dd4dd0a87dc665
Paulo Bandeira <paulounisul@gmail.com>
2022-01-12 07:49:33 -0300

    FutureBuilder -> Ok. FutureBuilder/Provider/Center/Consumer/ Refatorado orders_page para ser stateless e usar o futureBuilder  tendo o mesmo comportamento.

--- flutter/shop/lib/pages/orders_page.dart
+++ flutter/shop/lib/pages/orders_page.dart
@@ -4,43 +4,58 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatefulWidget {
-  const OrdersPage({Key? key}) : super(key: key);
+class OrdersPage extends StatelessWidget {
+  //Refatorado para FutureBuilder o ProdutoPage esta dessa da forma initState.
+  //caso queira fazer comparação dos modos.
 
-  @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
+  //bool _isLoading = true;
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
 
-class _OrdersPageState extends State<OrdersPage> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders().then((_) {
-      setState(() => _isLoading = false);
-    });
-  }
+  //   Provider.of<OrderList>(
+  //     context,
+  //     listen: false,
+  //   ).loadOrders().then((_) {
+  //     setState(() => _isLoading = false);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
+    //refatorado para futureBuilder
+    //final OrderList orders = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: orders.itemsCount,
-              itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
-            ),
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.error != null) {
+            return Center(child: Text('Occorreu sincronizando os Pedidos. '));
+          } else {
+            return Consumer<OrderList>(
+              builder: (ctx, orders, child) => ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+              ),
+            );
+          }
+        },
+      ),
+
+      // body: _isLoading
+      //     ? Center(child: CircularProgressIndicator())
+      //     : ListView.builder(
+      //         itemCount: orders.itemsCount,
+      //         itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+      //       ),
     );
   }
 }
