17573472c11637007df76777fad97316b3cacdc2
Paulo Bandeira <paulounisul@gmail.com>
2022-01-15 08:00:39 -0300

    tela de Autenticação #02 -> Ok /Container/Transform<Matrix4,rotationZ,translate>/BorderRatius/BoxShadow<Colors,OffSet>/TextStyle,Theme.of

--- flutter/shop/lib/utils/auth_page.dart
+++ flutter/shop/lib/utils/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
@@ -10,16 +12,49 @@ class AuthPage extends StatelessWidget {
         children: [
           Container(
             decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Color.fromRGBO(215, 117, 255, 0.5),
-                Color.fromRGBO(255, 188, 117, 0.9),
-              ]),
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(215, 117, 255, 0.5),
+                  Color.fromRGBO(255, 188, 117, 0.9),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
           ),
           Container(
             width: double.infinity,
             child: Column(
-              children: [],
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 70,
+                  ),
+                  //Cascate operator..
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.deepOrange.shade900,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(0, 3),
+                        ),
+                      ]),
+                  child: Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
