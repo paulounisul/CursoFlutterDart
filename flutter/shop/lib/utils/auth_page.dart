91584f6f05846a003022c27b126e76848f59870c
Paulo Bandeira <paulounisul@gmail.com>
2022-01-17 08:24:11 -0300

    Tela de Autenticação #03 -> Ok. /TextEditingController/Map<enum>/MediaQuery/RoundedRectangleBorder/Container/EdgeInsets/Column/TextFormField<decoration,keyboardType,obscureText,controller,onSaved,validator>/InputDecoration/TextInputType/SizeBox/ElevatedButton<styleFrom>/

--- flutter/shop/lib/utils/auth_page.dart
+++ flutter/shop/lib/utils/auth_page.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:shop/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -54,6 +55,7 @@ class AuthPage extends StatelessWidget {
                     ),
                   ),
                 ),
+                AuthForm(),
               ],
             ),
           ),
