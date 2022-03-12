48734dc269e7700c214cd3f9dde75f08000b5423
Paulo Bandeira <paulounisul@gmail.com>
2022-03-12 08:13:16 -0300

    Animando as Transições entre Telas. -> Ok /PageTransitionsBuilder/ThemeData<pageTransitionsTheme>/

--- flutter/shop/lib/main.dart
+++ flutter/shop/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/product_form_page.dart';
 //import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/utils/custom_route.dart';
 //import 'package:shop/utils/auth_page.dart';
 
 import 'models/cart.dart';
@@ -25,6 +26,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData tema = ThemeData(
       fontFamily: 'Lato',
+      pageTransitionsTheme: PageTransitionsTheme(builders: {
+        TargetPlatform.iOS: CustomPageTransitionsBuilder(),
+        TargetPlatform.android: CustomPageTransitionsBuilder(),
+      }),
     );
 
     return MultiProvider(
